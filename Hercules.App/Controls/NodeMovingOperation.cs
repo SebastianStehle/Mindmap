f810a453509c96145978a98da5a039aae0d83cb7
Sebastian <mail2stehle@gmail.com>
2015-11-09 16:20:34 +0100

    Bugfix in moving operation.

--- Hercules.App/Controls/NodeMovingOperation.cs
+++ Hercules.App/Controls/NodeMovingOperation.cs
@@ -89,7 +89,7 @@ namespace Hercules.App.Controls
         {
             try
             {
-                if ((initialPosition - movingNode.Position).LengthSquared() > 100)
+                if (movingNode != null && (initialPosition - movingNode.Position).LengthSquared() > 100)
                 {
                     AttachTarget target = layout.CalculateAttachTarget(document, renderer.Scene, targetNode, movingNode.Bounds);
 
@@ -98,6 +98,7 @@ namespace Hercules.App.Controls
                         targetNode.MoveTransactional(target.Parent, target.Index, target.NodeSide);
                     }
                 }
+
             }
             finally
             {
